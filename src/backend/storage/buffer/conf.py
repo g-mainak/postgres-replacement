48dbd30d5e886fb2f41a82b2cdc1bafdc0f4c35b
Henry Davidge <henrydavidge@gmail.com>
2013-02-13 13:18:36 -0500

    config script

---
+++ src/backend/storage/buffer/conf.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import os, sys, commands
+
+if len(sys.argv) != 2:
+	print "Need 2 args"
+else:
+	commands.getstatusoutput("cp -f bufmgr_" + sys.argv[1] + ".c bufmgr.c")
+	commands.getstatusoutput("cp -f freelist_" + sys.argv[1] + ".c freelist.c")
+	if sys.argv[1] == "orig":
+		commands.getstatusoutput("cp -f buf_init_orig.c buf_init.c")
+	else:
+		commands.getstatusoutput("cp -f buf_init_mod.c buf_init.c")
